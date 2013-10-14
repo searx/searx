cfba30f1ba555ffb3f145ff50a9646695fe1ea67
asciimoo <asciimoo@gmail.com>
2013-10-15 01:19:51 +0200

    [fix] comments removed

--- searx/engines/duckduckgo_definitions.py
+++ searx/engines/duckduckgo_definitions.py
@@ -17,16 +17,3 @@ def response(resp):
         results.append(base_result_template.format(**res))
 
     return results
-
-#from lxml import html
-#def request(query, params):
-#    params['method']    = 'POST'
-#    params['url']       = 'https://duckduckgo.com/html'
-#    params['data']['q'] = query
-#    return params
-#
-#
-#def response(resp):
-#    dom = html.fromstring(resp.text)
-#    results = dom.xpath('//div[@class="results_links results_links_deep web-result"]')
-#    return [html.tostring(x) for x in results]
