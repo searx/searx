26c9c0e8be632e91523b946d709e2752fe4c0441
asciimoo <asciimoo@gmail.com>
2013-10-15 00:19:25 +0200

    [enh] examples added

---
+++ examples/basic_engine.py
@@ -0,0 +1,21 @@
+
+def request(query, params):
+    '''pre-request callback
+    params<dict>:
+      method  : POST/GET
+      headers : {}
+      data    : {} # if method == POST
+      url     : ''
+    '''
+
+    params['url'] = 'https://host/%s' % query
+
+    return params
+
+
+def response(resp):
+    '''post-response callback
+    resp: requests response object
+    '''
+    return [resp.text]
+
