653a9f634862916be35e73cfe6d95a17d47dac6f
Okhin <okhin@datalove.me>
2013-10-25 18:36:38 +0200

    Add a lsearch engine for local search

---
+++ searx/engines/local.py
@@ -0,0 +1,12 @@
+from json import loads
+
+def request(query, params):
+    params['url'] = 'http://127.0.0.1:8888/lsearch?q=%s' % query
+    return params
+
+
+def response(resp):
+    results = loads(resp.text)
+    print results
+
+    return results
