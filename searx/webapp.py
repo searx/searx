dd1baa6d5894c814625cdf19e71b797ab6354d9c
pw3t <romain@berthor.fr>
2014-01-16 21:58:18 +0100

    [fix] replace the png icons by ico's, minor chnage in the html

--- searx/webapp.py
+++ searx/webapp.py
@@ -141,7 +141,7 @@ def index():
             result['pretty_url'] = result['url']
 
         for engine in result['engines']:
-            if engine in ['wikipedia', 'youtube', 'vimeo', 'soundcloud', 'twitter', 'stackoverflow']:
+            if engine in ['wikipedia', 'youtube', 'vimeo', 'soundcloud', 'twitter', 'stackoverflow', 'github']:
                 result['favicon'] = engine
             if engine in ['wikipedia', 'ddg definitions']:
                 featured_results.append(result)
