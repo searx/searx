137df2efefdba6bf20c9b3738923fd758bbbdac8
Adam Tauber <asciimoo@gmail.com>
2014-07-07 14:38:24 +0200

    [deploy] new pypi package (0.3.1)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = read('README.rst')
 
 setup(
     name='searx',
-    version="0.3.0",
+    version="0.3.1",
     description="A privacy-respecting, hackable metasearch engine",
     long_description=long_description,
     classifiers=[
