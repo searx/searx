80b9312e42087351bb081ceab717e479e75a1ab0
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2014-09-02 17:20:16 +0200

    add comments to dummy engine

--- searx/engines/dummy.py
+++ searx/engines/dummy.py
@@ -1,6 +1,14 @@
+## Dummy
+# 
+# @results     empty array
+# @stable      yes
+
+
+# do search-request
 def request(query, params):
     return params
 
 
+# get response from search-request
 def response(resp):
     return []
