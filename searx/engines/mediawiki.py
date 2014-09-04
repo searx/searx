35ddbecd744c51da3b0c5d40f8462925841473a4
Adam Tauber <asciimoo@gmail.com>
2014-09-04 23:53:13 +0200

    [fix] string formatting

--- searx/engines/mediawiki.py
+++ searx/engines/mediawiki.py
@@ -22,13 +22,13 @@ number_of_results = 1
 
 # search-url
 base_url = 'https://{language}.wikipedia.org/'
-search_url = base_url + 'w/api.php?action=query\
-                                  &list=search\
-                                  &{query}\
-                                  &srprop=timestamp\
-                                  &format=json\
-                                  &sroffset={offset}\
-                                  &srlimit={limit}'
+search_url = base_url + 'w/api.php?action=query'\
+                                 '&list=search'\
+                                 '&{query}'\
+                                 '&srprop=timestamp'\
+                                 '&format=json'\
+                                 '&sroffset={offset}'\
+                                 '&srlimit={limit}'
 
 
 # do search-request
@@ -37,7 +37,7 @@ def request(query, params):
     string_args = dict(query=urlencode({'srsearch': query}),
                         offset=offset,
                         limit=number_of_results)
-    format_strings = Formatter().parse(base_url)
+    format_strings = list(Formatter().parse(base_url))
 
     if params['language'] == 'all':
         language = 'en'
@@ -46,6 +46,7 @@ def request(query, params):
 
     if len(format_strings) > 1:
         string_args['language'] = language
+    print search_url.format(**string_args)
 
     # write search-language back to params, required in response
     params['language'] = language
