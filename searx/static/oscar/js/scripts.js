a538bed745a688d17c98372785dd2c0644e6019b
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2014-09-27 10:23:05 +0200

    oscar template: implement search input, index page,...

--- searx/static/oscar/js/scripts.js
+++ searx/static/oscar/js/scripts.js
@@ -12,20 +12,30 @@ $(document).ready(function(){
         var btnClass = 'btn-' + $(this).data('btn-class');
         var btnLabelDefault = $(this).data('btn-label-default');
         var btnLabelToggled = $(this).data('btn-label-toggled');
-        if(btnLabelToggled != '')
-        {
-            if($(this).hasClass('btn-default'))
-            {
+        if(btnLabelToggled != '') {
+            if($(this).hasClass('btn-default')) {
                 
                 var html = $(this).html().replace(btnLabelDefault, btnLabelToggled);
-            }
-            else
-            {
+            } else {
                 var html = $(this).html().replace(btnLabelToggled, btnLabelDefault);
             }
             $(this).html(html);
         }
         $(this).toggleClass(btnClass);
         $(this).toggleClass('btn-default');
+    });
+
+    $('.btn-collapse').click(function() {
+        var btnTextCollapsed = $(this).data('btn-text-collapsed');
+        var btnTextNotCollapsed = $(this).data('btn-text-not-collapsed');
+        
+        if(btnTextCollapsed != '' && btnTextNotCollapsed != '') {
+            if($(this).hasClass('collapsed')) {
+                var html = $(this).html().replace(btnTextCollapsed, btnTextNotCollapsed);
+            } else {
+                var html = $(this).html().replace(btnTextNotCollapsed, btnTextCollapsed);
+            }
+            $(this).html(html);
+        }
     });
 });
