c2e88339f7bb84e719e943b427a9afcdd84981aa
Cqoicebordel <Cqoicebordel@users.noreply.github.com>
2014-12-08 00:51:46 +0100

    Add pubdate
    Add pubdate with a good format, against publishedDate.

--- searx/webapp.py
+++ searx/webapp.py
@@ -294,10 +294,9 @@ def index():
 
         # TODO, check if timezone is calculated right
         if 'publishedDate' in result:
-            if result['publishedDate'].replace(tzinfo=None)\
-               >= datetime.now() - timedelta(days=1):
-                timedifference = datetime.now() - result['publishedDate']\
-                    .replace(tzinfo=None)
+            result['pubdate'] = result['publishedDate'].strftime('%Y-%m-%d %H:%M:%S%z')
+            if result['publishedDate'].replace(tzinfo=None) >= datetime.now() - timedelta(days=1):
+                timedifference = datetime.now() - result['publishedDate'].replace(tzinfo=None)
                 minutes = int((timedifference.seconds / 60) % 60)
                 hours = int(timedifference.seconds / 60 / 60)
                 if hours == 0:
@@ -305,8 +304,6 @@ def index():
                 else:
                     result['publishedDate'] = gettext(u'{hours} hour(s), {minutes} minute(s) ago').format(hours=hours, minutes=minutes)  # noqa
             else:
-                result['pubdate'] = result['publishedDate']\
-                    .strftime('%a, %d %b %Y %H:%M:%S %z')
                 result['publishedDate'] = format_date(result['publishedDate'])
 
     if search.request_data.get('format') == 'json':
