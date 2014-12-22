b975418e4ce33aef530f7ad88e100d47d73e4761
Adam Tauber <asciimoo@gmail.com>
2014-12-22 14:15:59 +0100

    [fix] flickr engine code cleanup ++ handle missing owner

--- searx/engines/flickr-noapi.py
+++ searx/engines/flickr-noapi.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-## Flickr (Images)
-# 
+#  Flickr (Images)
+#
 # @website     https://www.flickr.com
-# @provide-api yes (https://secure.flickr.com/services/api/flickr.photos.search.html) 
-# 
+# @provide-api yes (https://secure.flickr.com/services/api/flickr.photos.search.html)
+#
 # @using-api   no
 # @results     HTML
 # @stable      no
@@ -12,8 +12,6 @@
 
 from urllib import urlencode
 from json import loads
-from urlparse import urljoin
-from lxml import html
 import re
 
 categories = ['images']
@@ -22,11 +20,13 @@ url = 'https://secure.flickr.com/'
 search_url = url+'search/?{query}&page={page}'
 photo_url = 'https://www.flickr.com/photos/{userid}/{photoid}'
 regex = re.compile(r"\"search-photos-models\",\"photos\":(.*}),\"totalItems\":", re.DOTALL)
+image_sizes = ('o', 'k', 'h', 'b', 'c', 'z', 'n', 'm', 't', 'q', 's')
 
 paging = True
 
+
 def build_flickr_url(user_id, photo_id):
-    return photo_url.format(userid=user_id,photoid=photo_id)
+    return photo_url.format(userid=user_id, photoid=photo_id)
 
 
 def request(query, params):
@@ -37,58 +37,45 @@ def request(query, params):
 
 def response(resp):
     results = []
-    
+
     matches = regex.search(resp.text)
-    
-    if matches == None:
+
+    if matches is None:
         return results
 
     match = matches.group(1)
     search_results = loads(match)
-    
-    if not '_data' in search_results:
+
+    if '_data' not in search_results:
         return []
-    
+
     photos = search_results['_data']
-    
+
     for photo in photos:
-        
+
         # In paged configuration, the first pages' photos are represented by a None object
-        if photo == None:
+        if photo is None:
             continue
-        
+
+        img_src = None
         # From the biggest to the lowest format
-        if 'o' in photo['sizes']:
-            img_src = photo['sizes']['o']['displayUrl']
-        elif 'k' in photo['sizes']:
-            img_src = photo['sizes']['k']['displayUrl']
-        elif 'h' in photo['sizes']:
-            img_src = photo['sizes']['h']['displayUrl']
-        elif 'b' in photo['sizes']:
-            img_src = photo['sizes']['b']['displayUrl']
-        elif 'c' in photo['sizes']:
-            img_src = photo['sizes']['c']['displayUrl']
-        elif 'z' in photo['sizes']:
-            img_src = photo['sizes']['z']['displayUrl']
-        elif 'n' in photo['sizes']:
-            img_src = photo['sizes']['n']['displayUrl']
-        elif 'm' in photo['sizes']:
-            img_src = photo['sizes']['m']['displayUrl']
-        elif 't' in photo['sizes']:
-            img_src = photo['sizes']['to']['displayUrl']
-        elif 'q' in photo['sizes']:
-            img_src = photo['sizes']['q']['displayUrl']
-        elif 's' in photo['sizes']:
-            img_src = photo['sizes']['s']['displayUrl']
-        else:
+        for image_size in image_sizes:
+            if image_size in photo['sizes']:
+                img_src = photo['sizes'][image_size]['displayUrl']
+                break
+
+        if not img_src:
             continue
-        
+
+        if 'id' not in photo['owner']:
+            continue
+
         url = build_flickr_url(photo['owner']['id'], photo['id'])
 
         title = photo['title']
-        
-        content = '<span class="photo-author">'+ photo['owner']['username'] +'</span><br />'
-        
+
+        content = '<span class="photo-author">' + photo['owner']['username'] + '</span><br />'
+
         if 'description' in photo:
             content = content + '<span class="description">' + photo['description'] + '</span>'
 
@@ -98,5 +85,5 @@ def response(resp):
                         'img_src': img_src,
                         'content': content,
                         'template': 'images.html'})
-        
+
     return results
