829948b85df0510e331372bcd60cb31db9c96a5c
Cqoicebordel <Cqoicebordel@users.noreply.github.com>
2014-12-23 01:41:25 +0100

    Add language support Allow the user to select a language. It must be written in english, and capitalized, ie : English, French, German, Hungarian...

--- searx/engines/subtitleseeker.py
+++ searx/engines/subtitleseeker.py
@@ -16,6 +16,8 @@ from lxml import html
 categories = ['videos']
 paging = True
 
+language = ""
+
 # search-url
 url = 'http://www.subtitleseeker.com/'
 search_url = url+'search/TITLES/{query}&p={pageno}'
@@ -41,6 +43,10 @@ def response(resp):
     for result in dom.xpath(results_xpath):
         link = result.xpath(".//a")[0]
         href = link.attrib.get('href')
+        
+        if language is not "":
+            href = href + language + "/"
+
         title = escape(link.xpath(".//text()")[0])
 
         content = result.xpath('.//div[contains(@class,"red")]//text()')[0]
