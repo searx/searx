4d0aeae567e19e68747fd2869741fee11a4df7c4
Cqoicebordel <Cqoicebordel@users.noreply.github.com>
2015-01-19 22:17:12 +0100

    Thanks @pointhi !

--- searx/autocomplete.py
+++ searx/autocomplete.py
@@ -49,17 +49,17 @@ def searx_bang(full_query):
             # check if query starts with categorie name
             for categorie in categories:
                 if categorie.startswith(engine_query):
-                    results.append('!{categorie}'.format(categorie=categorie))
+                    results.append(first_char+'{categorie}'.format(categorie=categorie))
 
             # check if query starts with engine name
             for engine in engines:
                 if engine.startswith(engine_query.replace('_', ' ')):
-                    results.append('!{engine}'.format(engine=engine.replace(' ', '_')))
+                    results.append(first_char+'{engine}'.format(engine=engine.replace(' ', '_')))
 
             # check if query starts with engine shortcut
             for engine_shortcut in engine_shortcuts:
                 if engine_shortcut.startswith(engine_query):
-                    results.append('!{engine_shortcut}'.format(engine_shortcut=engine_shortcut))
+                    results.append(first_char+'{engine_shortcut}'.format(engine_shortcut=engine_shortcut))
 
     # check if current query stats with :bang
     elif first_char == ':':
