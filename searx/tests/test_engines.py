4dba3739fb3b98572cbd51adab226376b5844105
Cqoicebordel <Cqoicebordel@users.noreply.github.com>
2015-01-26 18:24:08 +0100

    Youtube's unit test

--- searx/tests/test_engines.py
+++ searx/tests/test_engines.py
@@ -4,3 +4,4 @@ from searx.tests.engines.test_dummy import *  # noqa
 from searx.tests.engines.test_flickr import *  # noqa
 from searx.tests.engines.test_github import *  # noqa
 from searx.tests.engines.test_mixcloud import *  # noqa
+from searx.tests.engines.test_youtube import *  # noqa
