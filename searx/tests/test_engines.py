8f040e30adbbd615155a5075bec28ccadff10eff
Cqoicebordel <Cqoicebordel@users.noreply.github.com>
2015-01-26 17:36:10 +0100

    Flickr's test unit

--- searx/tests/test_engines.py
+++ searx/tests/test_engines.py
@@ -1,5 +1,6 @@
 from searx.tests.engines.test_bing import *  # noqa
 from searx.tests.engines.test_deezer import *  # noqa
 from searx.tests.engines.test_dummy import *  # noqa
+from searx.tests.engines.test_flickr import *  # noqa
 from searx.tests.engines.test_github import *  # noqa
 from searx.tests.engines.test_mixcloud import *  # noqa
