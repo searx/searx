10666fd7c03ac3e9ba6507d36f04181fb90ae466
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2015-02-08 22:15:25 +0100

    [enh] add safesearch to google_images

--- searx/engines/google_images.py
+++ searx/engines/google_images.py
@@ -15,18 +15,25 @@ from json import loads
 # engine dependent config
 categories = ['images']
 paging = True
+safesearch = True
 
 # search-url
 url = 'https://ajax.googleapis.com/'
-search_url = url + 'ajax/services/search/images?v=1.0&start={offset}&rsz=large&safe=off&filter=off&{query}'
+search_url = url + 'ajax/services/search/images?v=1.0&start={offset}&rsz=large&safe={safesearch}&filter=off&{query}'
 
 
 # do search-request
 def request(query, params):
     offset = (params['pageno'] - 1) * 8
 
+    if params['safesearch'] == 2:
+        safesearch = 'on'
+    else:
+        safesearch = 'off'
+
     params['url'] = search_url.format(query=urlencode({'q': query}),
-                                      offset=offset)
+                                      offset=offset,
+                                      safesearch=safesearch)
 
     return params
 
