13ea0a20ae222c51d7aac6f751124e484bc1cae3
Adam Tauber <asciimoo@gmail.com>
2015-03-14 19:45:15 +0100

    [enh] gettext in self ip plugin

--- searx/plugins/self_ip.py
+++ searx/plugins/self_ip.py
@@ -1,9 +1,12 @@
-
+from flask.ext.babel import gettext
 name = "Self IP"
-description = "Display your source IP address"
+description = gettext('Display your source IP address if the query expression is "ip"')
 default_on = True
 
 
+# attach callback to the pre search hook
+#  request: flask request object
+#  ctx: the whole local context of the pre search hook
 def pre_search(request, ctx):
     if ctx['search'].query == 'ip':
         x_forwarded_for = request.headers.getlist("X-Forwarded-For")
@@ -13,5 +16,6 @@ def pre_search(request, ctx):
             ip = request.remote_addr
         ctx['search'].answers.clear()
         ctx['search'].answers.add(ip)
+        # return False prevents exeecution of the original block
         return False
     return True
