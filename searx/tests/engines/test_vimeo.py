ab273e2faaa0ad157443ffe0b09194fad17ad743
Cqoicebordel <Cqoicebordel@users.noreply.github.com>
2015-06-12 19:53:38 +0200

    Fix Vimeo engine. Solve #368

--- searx/tests/engines/test_vimeo.py
+++ searx/tests/engines/test_vimeo.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from collections import defaultdict
 import mock
 from searx.engines import vimeo
@@ -25,26 +26,42 @@ class TestVimeoEngine(SearxTestCase):
         self.assertEqual(vimeo.response(response), [])
 
         html = """
-        <div id="browse_content" class="" data-search-id="696d5f8366914ec4ffec33cf7652de384976d4f4">
-            <ol class="js-browse_list clearfix browse browse_videos browse_videos_thumbnails kane"
+        <div id="browse_content" class="results_grid" data-search-id="696d5f8366914ec4ffec33cf7652de384976d4f4">
+            <ul class="js-browse_list clearfix browse browse_videos browse_videos_thumbnails kane"
                 data-stream="c2VhcmNoOjo6ZGVzYzp7InF1ZXJ5IjoidGVzdCJ9">
-                <li id="clip_100785455" data-start-page="/search/page:1/sort:relevant/" data-position="1">
-                    <a href="/videoid" title="Futurama 3d (test shot)">
-                        <img src="http://image.url.webp"
-                            srcset="http://i.vimeocdn.com/video/482375085_590x332.webp 2x" alt=""
-                            class="thumbnail thumbnail_lg_wide">
-                        <div class="data">
-                            <p class="title">
-                                This is the title
-                            </p>
-                            <p class="meta">
-                                <time datetime="2014-07-15T04:16:27-04:00"
-                                    title="mardi 15 juillet 2014 04:16">Il y a 6 mois</time>
-                            </p>
-                        </div>
-                    </a>
+                <li data-position="7" data-result-id="clip_79600943">
+                    <div class="clip_thumbnail">
+                        <a href="/videoid" class="js-result_url">
+                            <div class="thumbnail_wrapper">
+                                <img src="http://image.url.webp" class="js-clip_thumbnail_image">
+                                <div class="overlay overlay_clip_meta">
+                                    <div class="meta_data_footer">
+                                        <span class="clip_upload_date">
+                                            <time datetime="2013-11-17T08:49:09-05:00"
+                                                title="dimanche 17 novembre 2013 08:49">Il y a 1 an</time>
+                                        </span>
+                                        <span class="clip_likes">
+                                            <img src="https://f.vimeocdn.com/images_v6/svg/heart-icon.svg">2 215
+                                        </span>
+                                        <span class="clip_comments">
+                                            <img src="https://f.vimeocdn.com/images_v6/svg/comment-icon.svg">75
+                                        </span>
+                                        <span class="overlay meta_data_footer clip_duration">01:12</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="title">This is the title</span>
+                        </a>
+                    </div>
+                    <div class="clip_thumbnail_attribution">
+                        <a href="/fedorshmidt">
+                            <img src="https://i.vimeocdn.com/portrait/6628061_100x100.jpg" class="avatar">
+                            <span class="display_name">Fedor Shmidt</span>
+                        </a>
+                        <span class="plays">2,1M lectures</span>
+                    </div>
                 </li>
-            </ol>
+            </ul>
         </div>
         """
         response = mock.Mock(text=html)
