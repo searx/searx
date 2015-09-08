1f06d390bf5a1af6dd6ed9c0bf25c0a413448943
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2015-09-08 17:44:41 +0200

    [enh] add tests for spellchecker plugin

--- searx/tests/test_plugins.py
+++ searx/tests/test_plugins.py
@@ -2,6 +2,7 @@
 
 from searx.testing import SearxTestCase
 from searx import plugins
+from searx.plugins.spellchecker import levenshtein, spell_corrections, corrections_from_suggestions
 from mock import Mock
 
 
@@ -66,3 +67,57 @@ class SelfIPTest(SearxTestCase):
         ctx = {'search': Mock(answers=set(),
                               query='What is my User-Agent?')}
         store.call('post_search', request, ctx)
+
+
+class SpellCheckerTest(SearxTestCase):
+
+    def test_SpellChecker_init(self):
+        store = plugins.PluginStore()
+        store.register(plugins.spellchecker)
+
+        self.assertTrue(len(store.plugins) == 1)
+
+    def test_levenshtein(self):
+        self.assertEqual(levenshtein("", ""), 0)
+        self.assertEqual(levenshtein("hello", "hello"), 0)
+        self.assertEqual(levenshtein("", "hello"), 5)
+        self.assertEqual(levenshtein("hello", ""), 5)
+        self.assertEqual(levenshtein("kitten", "sitting"), 3)
+        self.assertEqual(levenshtein(u"m\u00FChle", "muehle"), 2)
+        self.assertEqual(levenshtein(u"Linux", "linux"), 1)
+
+    def test_spell_corrections(self):
+        directory = ("annotate", "annotated", "annotates", "annotating",
+                     "annotation", "annotations", "announce", "announced",
+                     "announcement", "announcements", "announcer", "announcers",
+                     "announces", "announcing", "annoy", "annoyance")
+
+        corrections = spell_corrections("annotatd", directory, 3)
+        self.assertEqual(len(corrections), 5)
+        self.assertTrue('annotate' in corrections)
+        self.assertTrue('annotated' in corrections)
+        self.assertTrue('annotates' in corrections)
+        self.assertTrue('annotating' in corrections)
+        self.assertTrue('annotation' in corrections)
+
+        corrections = spell_corrections("announcement", directory, 3)
+        self.assertEqual(len(corrections), 2)
+        self.assertTrue('announcement' in corrections)
+        self.assertTrue('announcements' in corrections)
+
+        corrections = spell_corrections("hello", directory, 3)
+        self.assertEqual(len(corrections), 0)
+
+    def test_corrections_from_suggestions(self):
+        suggestions = ("a privacy respecting, hackable metasearch engine",
+                       "python based search engine", "open source software")
+
+        corrections = corrections_from_suggestions("opn sorce serch engine", suggestions)
+        self.assertEqual(len(corrections), 1)
+        self.assertTrue("open source search engine" in corrections)
+
+        corrections = corrections_from_suggestions("open source search engine", suggestions)
+        self.assertEqual(len(corrections), 0)
+
+        corrections = corrections_from_suggestions("openstreetma", suggestions)
+        self.assertEqual(len(corrections), 0)
