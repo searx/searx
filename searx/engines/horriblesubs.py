20178ba93191c3c9020c5f68f2aa18a80990a5eb
Yuyu <admin@yuyu.io>
2016-09-09 11:41:27 +0200

    Added support to show current shows release time

--- searx/engines/horriblesubs.py
+++ searx/engines/horriblesubs.py
@@ -10,10 +10,13 @@
 """
 
 from cgi import escape
-from urllib import urlencode
+from urllib import urlencode, unquote_plus
+from urlparse import urljoin
 from lxml import html
 from lxml.etree import XMLSyntaxError
+import re
 from searx.engines.xpath import extract_text
+from searx.poolrequests import get as http_get
 
 # engine dependent config
 categories = ['files', 'videos']
@@ -21,6 +24,7 @@ paging = True
 
 # search-url
 base_url = 'http://horriblesubs.info/'
+schedules_url = 'http://horriblesubs.info/release-schedule/'
 search_url = base_url + 'lib/search.php?{query}&nextid={offset}'
 
 # xpath queries
@@ -30,6 +34,55 @@ xpath_magnetlink = './/td[contains(@class, "hs-magnet-link")]/span/a'
 xpath_torrentfile = './/td[contains(@class, "hs-torrent-link")]/span/a'
 xpath_ddls = './/td[contains(@class, "hs-ddl-link")]/span/a'
 
+xpath_schedule_weekdays = '//h2[contains(@class, "weekday")]'
+xpath_schedule_shows = './/tr[contains(@class, "schedule-page-item")]'
+xpath_schedule_show_title = './/td[contains(@class, "schedule-page-show")]/a'
+xpath_schedule_show_time = './/td[contains(@class, "schedule-time")]'
+
+def index_schedules():
+    schedules = []
+
+    resp = http_get(schedules_url)
+    dom = html.fromstring(resp.text)
+
+    for weekday in dom.xpath(xpath_schedule_weekdays):
+        day = weekday.text_content()
+
+        weekday_table = weekday.getnext()
+        shows = []
+
+        for show in weekday_table.xpath(xpath_schedule_shows):
+            title = show.xpath(xpath_schedule_show_title)[0]
+            href = urljoin(base_url, title.attrib.get('href'))
+            title = title.text_content()
+            time = show.xpath(xpath_schedule_show_time)[0].text_content()
+            shows.append({'title': title,
+                          'url': href,
+                          'time': time})
+
+        schedules.append({'weekday': day,
+                          'shows': shows})
+
+    return schedules
+
+schedules = index_schedules()
+
+# search for a schedule entry
+def search_schedule(search):
+    search = search.lower()
+    result = None
+
+    for weekday in schedules:
+        if result: break
+
+        for show in weekday['shows']:
+            if result: break
+
+            if search in show['title'].lower():
+                result = show
+                result['weekday'] = weekday['weekday']
+
+    return result
 
 # do search-request
 def request(query, params):
@@ -41,6 +94,15 @@ def request(query, params):
 # get response from search-request
 def response(resp):
     results = []
+    query = unquote_plus(re.search(r'value=(.*?)&', resp.url).groups()[0])
+    show = search_schedule(query)
+
+    if show:
+        content = 'Releases at {weekday} {time} PST'
+        content = content.format(weekday=show['weekday'], time=show['time'])
+        results.append({'url': show['url'],
+                        'title': show['title'],
+                        'content': content})
 
     try:
         dom = html.fromstring(resp.text)
@@ -49,7 +111,7 @@ def response(resp):
 
     for result in dom.xpath(xpath_results):
         title = result.xpath(xpath_title)[0].text_content()
-        content = "Resolution: " + title[title.index('[')+1:title.index(']')]
+        content = 'Resolution: ' + title[title.index('[')+1:title.index(']')]
 
         magnetlink = result.xpath(xpath_magnetlink)[0].attrib.get('href')
 
