ddb0e2168287df97bea7652eb244a8f85d0acc6e
dalf <alex@al-f.net>
2016-10-25 22:46:31 +0200

    [fix] erase temporary jinja compiled file with searx exit

--- searx/jinja_compile.py
+++ searx/jinja_compile.py
@@ -1,18 +1,19 @@
 import jinja2
 import tempfile
-
+import weakref
 from searx import logger
+
 logger = logger.getChild('jinja_compile')
+archives = weakref.WeakKeyDictionary()
 
 
 def compile(app):
+    archive = tempfile.NamedTemporaryFile(prefix='searx', suffix='.zip')
     env = app.jinja_env
-    # archive = tempfile.TemporaryFile()
-    _, archive = tempfile.mkstemp(suffix='.zip', prefix='searx')
-    # archive.close = dummy
     env.compile_templates(archive,
                           zip='stored',
                           py_compile=True,
                           log_function=logger.debug,
                           ignore_errors=False)
-    app.jinja_env = env.overlay(loader=jinja2.loaders.ModuleLoader(archive))
+    app.jinja_env = env.overlay(loader=jinja2.loaders.ModuleLoader(archive.name))
+    archives[app] = archive
