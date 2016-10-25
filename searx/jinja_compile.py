1bc03b54d394477af47fb6f0e5b3490f7b0b04c2
dalf <alex@al-f.net>
2016-10-25 15:50:23 +0200

    [fix] PEP8 searx/jinja_compile.py

--- searx/jinja_compile.py
+++ searx/jinja_compile.py
@@ -4,14 +4,15 @@ import tempfile
 from searx import logger
 logger = logger.getChild('jinja_compile')
 
+
 def compile(app):
     env = app.jinja_env
     # archive = tempfile.TemporaryFile()
     _, archive = tempfile.mkstemp(suffix='.zip', prefix='searx')
     # archive.close = dummy
-    env.compile_templates(archive, 
+    env.compile_templates(archive,
                           zip='stored',
                           py_compile=True,
                           log_function=logger.debug,
                           ignore_errors=False)
-    app.jinja_env= env.overlay(loader=jinja2.loaders.ModuleLoader(archive))
+    app.jinja_env = env.overlay(loader=jinja2.loaders.ModuleLoader(archive))
