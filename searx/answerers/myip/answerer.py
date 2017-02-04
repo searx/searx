8f379181cc253e1ee567b34ab74d469f14e5b33b
conmarap <conmarap@gmail.com>
2017-02-04 11:53:05 -0500

    Code style cahnges

--- searx/answerers/myip/answerer.py
+++ searx/answerers/myip/answerer.py
@@ -7,11 +7,12 @@ from searx import settings
 # Specifies which search query keywords triggers this answerer
 keywords = ('myip',)
 
+
 # Returns a string with all the information retrieved from ip-api.com's API
 def get_answer():
     # Find the outgoing proxies settings from the user configuration.
     outgoing_proxies = settings['outgoing'].get('proxies', None)
-	
+
     # Initiate a GET request and set the outgoing proxies, if any were set in
     # settings.yml.
     ip_info = get('http://ip-api.com/json', proxies=outgoing_proxies).json()
@@ -23,12 +24,14 @@ def get_answer():
         ip_info['country'],
         ip_info['isp'])
 
+
 # Required answerer function
 def answer(query):
     try:
         return [{'answer': get_answer()}]
     except:
         return []
+
 
 # Required answerer function
 def self_info():
