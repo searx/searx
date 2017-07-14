af299b308e943eff5f316c6df698996949bb2460
jibe-b <user701@orange.fr>
2017-07-14 20:47:36 +0200

    Add XML parser to fix encoding error
    Traceback of the corresponding error:
    
    >ERROR:searx.search:engine base : exception : Unicode strings with encoding declaration are not supported. Please use bytes input or XML fragments without declaration.
    Traceback (most recent call last):
      File "/home/u/abc/searx/searx/search.py", line 117, in search_one_request_safe
        search_results = search_one_request(engine, query, request_params, start_time, timeout_limit)
      File "/home/u/abc/searx/searx/search.py", line 109, in search_one_request
        return engine.response(response)
      File "/home/u/abc/searx/searx/engines/base.py", line 76, in response
        search_results = etree.XML(resp.text)
      File "src/lxml/lxml.etree.pyx", line 3192, in lxml.etree.XML (src/lxml/lxml.etree.c:77490)
      File "src/lxml/parser.pxi", line 1825, in lxml.etree._parseMemoryDocument (src/lxml/lxml.etree.c:116615)
    ValueError: Unicode strings with encoding declaration are not supported. Please use bytes input or XML fragments without declaration.

--- searx/engines/base.py
+++ searx/engines/base.py
@@ -73,7 +73,8 @@ def request(query, params):
 def response(resp):
     results = []
 
-    search_results = etree.XML(resp.text)
+    parser = etree.XMLParser()
+    search_results = etree.XML(resp.text), parser)
 
     for entry in search_results.xpath('./result/doc'):
         content = "No description available"
