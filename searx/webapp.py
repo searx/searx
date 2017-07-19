f2b13a7de071cd68c9aeef7ff3e87ede88e677e8
Adam Tauber <asciimoo@gmail.com>
2017-07-19 21:33:44 +0200

    [enh] allow default empty values for normal and result proxies

--- searx/webapp.py
+++ searx/webapp.py
@@ -142,7 +142,7 @@ _category_names = (gettext('files'),
                    gettext('map'),
                    gettext('science'))
 
-outgoing_proxies = settings['outgoing'].get('proxies', None)
+outgoing_proxies = settings['outgoing'].get('proxies') or None
 
 
 @babel.localeselector
@@ -353,7 +353,7 @@ def render(template_name, override_theme=None, **kwargs):
 
     kwargs['image_proxify'] = image_proxify
 
-    kwargs['proxify'] = proxify if settings.get('result_proxy') else None
+    kwargs['proxify'] = proxify if settings.get('result_proxy', {}).get('url') else None
 
     kwargs['get_result_template'] = get_result_template
 
