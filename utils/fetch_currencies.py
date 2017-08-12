31a5540f00d0e87df69426cf634b42949903b0f1
Alexandre Flament <alex@al-f.net>
2017-08-12 19:32:35 +0200

    [mod] update currencies.json and fetch_currencies.py

--- utils/fetch_currencies.py
+++ utils/fetch_currencies.py
@@ -3,18 +3,23 @@ import json
 import re
 import unicodedata
 import string
+import io
 from urllib import urlencode
 from requests import get
 
-languages = {'de', 'en', 'es', 'fr', 'hu', 'it', 'nl', 'jp'}
+query = """# ?item (LANG(?label) AS ?lang) 
+SELECT DISTINCT ?iso4217 ?unit ?unicode ?label ?alias WHERE {
+  ?item wdt:P498 ?iso4217; rdfs:label ?label.
+  OPTIONAL { ?item skos:altLabel ?alias FILTER (LANG (?alias) = LANG(?label)). }
+  OPTIONAL { ?item wdt:P558 ?unit. }
+  OPTIONAL { ?item wdt:P489 ?symbol.
+             ?symbol wdt:P487 ?unicode. }
+  FILTER(LANG(?label) IN ('de', 'en', 'es', 'fr', 'hu', 'it', 'nl', 'jp', 'hu', 'bg', 'cs', 'el', 'eo', 'fi', 'he', 'it', 'nl', 'pt', 'ro', 'ru', 'sk', 'sv', 'tr', 'uk', 'zh')).
+}
+ORDER BY ?item ?lang
+"""
 
-url_template = 'https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&{query}&props=labels%7Cdatatype%7Cclaims%7Caliases&languages=' + '|'.join(languages)
-url_wmflabs_template = 'http://wdq.wmflabs.org/api?q='
-url_wikidata_search_template = 'http://www.wikidata.org/w/api.php?action=query&list=search&format=json&srnamespace=0&srprop=sectiontitle&{query}'
-
-wmflabs_queries = [
-    'CLAIM[31:8142]',  # all devise
-]
+sparql_endpoint_url = 'https://query.wikidata.org/sparql'
 
 db = {
     'iso4217': {
@@ -29,6 +34,8 @@ def remove_accents(data):
 
 
 def normalize_name(name):
+    # remove right to left character (first to trim spaces after)
+    name = filter(lambda c: unicodedata.category(c) != 'Cf', name)
     return re.sub(' +', ' ', remove_accents(name.lower()).replace('-', ' '))
 
 
@@ -61,91 +68,37 @@ def add_currency_label(label, iso4217, language):
     db['iso4217'][iso4217][language] = label
 
 
-def get_property_value(data, name):
-    prop = data.get('claims', {}).get(name, {})
-    if len(prop) == 0:
-        return None
-
-    value = prop[0].get('mainsnak', {}).get('datavalue', {}).get('value', '')
-    if value == '':
-        return None
-
-    return value
+def get_value(binding, key):
+    return unicode(binding.get(key, {}).get('value', None))
 
 
-def parse_currency(data):
-    iso4217 = get_property_value(data, 'P498')
+def wd_query():
+    url = sparql_endpoint_url + '?' + urlencode({'format': 'json', 'query': query})
+    htmlresponse = get(url)
+    jsonresponse = json.loads(htmlresponse.content, encoding="utf-8")
+    for c in jsonresponse.get('results', {}).get('bindings', {}):
+        iso4217 = get_value(c, 'iso4217')
+        unit = get_value(c, 'unit')
+        unit_unicode = get_value(c, 'unicode')
+        label = get_value(c, 'label')
+        alias = get_value(c, 'alias')
+        language = c.get('label', {}).get('xml:lang', None)
 
-    if iso4217 is not None:
-        unit = get_property_value(data, 'P558')
+        if label is not None:
+            add_currency_label(label, iso4217, language)
+            add_currency_name(label, iso4217)
+
+        if alias is not None:
+            add_currency_name(alias, iso4217)
+
+        if unit_unicode is not None:
+            add_currency_name(unit_unicode, iso4217)
+
         if unit is not None:
             add_currency_name(unit, iso4217)
 
-        labels = data.get('labels', {})
-        for language in languages:
-            name = labels.get(language, {}).get('value', None)
-            if name is not None:
-                add_currency_name(name, iso4217)
-                add_currency_label(name, iso4217, language)
 
-        aliases = data.get('aliases', {})
-        for language in aliases:
-            for i in range(0, len(aliases[language])):
-                alias = aliases[language][i].get('value', None)
-                add_currency_name(alias, iso4217)
-
-
-def fetch_data(wikidata_ids):
-    url = url_template.format(query=urlencode({'ids': '|'.join(wikidata_ids)}))
-    htmlresponse = get(url)
-    jsonresponse = json.loads(htmlresponse.content)
-    entities = jsonresponse.get('entities', {})
-
-    for pname in entities:
-        pvalue = entities.get(pname)
-        parse_currency(pvalue)
-
-
-def add_q(i):
-    return "Q" + str(i)
-
-
-def fetch_data_batch(wikidata_ids):
-    while len(wikidata_ids) > 0:
-        if len(wikidata_ids) > 50:
-            fetch_data(wikidata_ids[0:49])
-            wikidata_ids = wikidata_ids[50:]
-        else:
-            fetch_data(wikidata_ids)
-            wikidata_ids = []
-
-
-def wdq_query(query):
-    url = url_wmflabs_template + query
-    htmlresponse = get(url)
-    jsonresponse = json.loads(htmlresponse.content)
-    qlist = map(add_q, jsonresponse.get('items', {}))
-    error = jsonresponse.get('status', {}).get('error', None)
-    if error is not None and error != 'OK':
-        print "error for query '" + query + "' :" + error
-
-    fetch_data_batch(qlist)
-
-
-def wd_query(query, offset=0):
-    qlist = []
-
-    url = url_wikidata_search_template.format(query=urlencode({'srsearch': query, 'srlimit': 50, 'sroffset': offset}))
-    htmlresponse = get(url)
-    jsonresponse = json.loads(htmlresponse.content)
-    for r in jsonresponse.get('query', {}).get('search', {}):
-        qlist.append(r.get('title', ''))
-    fetch_data_batch(qlist)
-
-
-# fetch #
-for q in wmflabs_queries:
-    wdq_query(q)
+wd_query()
 
 # static
 add_currency_name(u"euro", 'EUR')
@@ -156,6 +109,6 @@ add_currency_name(u"peso", 'MXN')
 add_currency_name(u"pesos", 'MXN')
 
 # write
-f = open("currencies.json", "wb")
-json.dump(db, f, indent=4, encoding="utf-8")
-f.close()
+with io.open("currencies.json", "w", encoding='utf8') as f:
+    j = json.dumps(db, encoding="utf-8", ensure_ascii=False)
+    f.write(j)
