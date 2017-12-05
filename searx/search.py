1435ae7e96984eabd1df43095d27f21e5a5d424a
Adam Tauber <asciimoo@gmail.com>
2017-12-05 20:38:32 +0100

    [enh] display engine error messages

--- searx/search.py
+++ searx/search.py
@@ -135,7 +135,10 @@ def search_one_request_safe(engine_name, query, request_params, result_container
                              .format(engine_name, search_duration, timeout_limit, e))
             requests_exception = True
         else:
-            result_container.add_unresponsive_engine((engine_name, gettext('unexpected crash')))
+            result_container.add_unresponsive_engine((
+                engine_name,
+                u'{0}: {1}'.format(gettext('unexpected crash'), e),
+            ))
             # others errors
             logger.exception('engine {0} : exception : {1}'.format(engine_name, e))
 
