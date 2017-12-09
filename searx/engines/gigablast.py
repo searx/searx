7752b0a8075b8813c3e74aebdd9655aecdf38444
Adam Tauber <asciimoo@gmail.com>
2017-12-09 21:00:08 +0100

    [fix] gigablast url parameter change - fixes #1107

--- searx/engines/gigablast.py
+++ searx/engines/gigablast.py
@@ -33,7 +33,7 @@ search_string = 'search?{query}'\
     '&qh=0'\
     '&qlang={lang}'\
     '&ff={safesearch}'\
-    '&rxieu={rxieu}'\
+    '&rxiec={rxieu}'\
     '&rand={rxikd}'  # current unix timestamp
 
 # specific xpath variables
