ddcb619b9702dfd10bc89145d35e3ed32ac11077
frankdelange <github-f@unternet.org>
2018-04-03 23:35:27 +0200

    - recoll engine
       recoll is a local search engine based on Xapian:
       http://www.lesbonscomptes.com/recoll/
    
       By itself recoll does not offer web or API access,
       this can be achieved using recoll-webui:
       https://github.com/koniu/recoll-webui
    
       As recoll-webui by default does not support paged JSON
       results it is advisable to use a patched version which does:
       https://github.com/Yetangitu/recoll-webui/tree/jsonpage
       A pull request was sent upstream, if this is merged the patched
       version is no longer needed
    
       This engine uses a custom 'files' result template
    
       set base_url to the location where recoll-webui can be reached
       set dl_prefix to a location where the file hierarchy as indexed by recoll can be reached
       set search_dir to the part of the indexed file hierarchy to be searched, use an empty string to search the entire search domain

---
+++ searx/engines/recoll.py
@@ -0,0 +1,75 @@
+"""
+ Recoll (local search engine)
+
+ @using-api   yes
+ @results     JSON
+ @stable      yes
+ @parse       url, content, size, abstract, author, mtype, time, filename, label
+"""
+
+from json import loads
+from searx.url_utils import urlencode
+from datetime import date, timedelta
+
+# engine dependent config
+paging = True
+time_range_support = True
+
+# parameters from settings.yml
+base_url = None
+search_dir = ''
+dl_prefix = None
+
+def get_time_range(time_range):
+    sw = {
+	    'day' : 1,
+	    'week': 7,
+	    'month': 30,
+	    'year': 365
+    }
+
+    offset = sw.get(time_range,0)
+    if not offset:
+	    return ''
+
+    return (date.today()-timedelta(days=offset)).isoformat()
+
+# do search-request
+def request(query, params):
+    search_after = get_time_range(params['time_range'])
+    search_url = base_url + 'pagedjson?query={query}&page={page}&after={after}&dir={dir}'
+
+    params['url'] = search_url.format(query=urlencode({'q': query}), page=params['pageno'], after=search_after, dir=search_dir)
+
+    return params
+
+# get response from search-request
+def response(resp):
+    results = []
+
+    raw_search_results = loads(resp.text)
+
+    if not raw_search_results:
+        return []
+
+    for result in raw_search_results.get('results', []):
+        title = result['label']
+        url = result['url'].replace('file:///export', dl_prefix)
+        content = u'{}'.format(result['snippet'])
+
+        # append result
+        item = {'url': url,
+                'title': title,
+                'content': content,
+                'template': 'files.html'}
+
+        if result['size']:
+            item['size'] = int(result['size'])
+
+        for parameter in ['filename', 'abstract', 'author', 'mtype', 'time']:
+            if result[parameter]:
+                item[parameter] = result[parameter]
+
+        results.append(item)
+
+    return results
