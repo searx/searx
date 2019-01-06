0d4d58f794fea3a4439f4ed4689e52b8031e5246
Étienne Deparis <etienne@depar.is>
2019-01-06 18:38:51 +0100

    [enh] Add preference section to two upstream plugins for simple theme integration

--- searx/plugins/vim_hotkeys.py
+++ searx/plugins/vim_hotkeys.py
@@ -5,6 +5,7 @@ description = gettext('Navigate search results with Vim-like hotkeys '
                       '(JavaScript required). '
                       'Press "h" key on main or result page to get help.')
 default_on = False
+preference_section = 'ui'
 
 js_dependencies = ('plugins/js/vim_hotkeys.js',)
 css_dependencies = ('plugins/css/vim_hotkeys.css',)
