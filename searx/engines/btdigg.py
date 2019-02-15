e7db300147c76b47c502cc61acdc691fe511c24e
volth <volth@volth.com>
2019-02-15 07:39:29 +0000

    restore btdigg as btdig.com

--- searx/engines/btdigg.py
+++ searx/engines/btdigg.py
@@ -1,7 +1,7 @@
 """
  BTDigg (Videos, Music, Files)
 
- @website     https://btdigg.org
+ @website     https://btdig.com
  @provide-api yes (on demand)
 
  @using-api   no
@@ -21,7 +21,7 @@ categories = ['videos', 'music', 'files']
 paging = True
 
 # search-url
-url = 'https://btdigg.org'
+url = 'https://btdig.com'
 search_url = url + '/search?q={search_term}&p={pageno}'
 
 
@@ -39,7 +39,7 @@ def response(resp):
 
     dom = html.fromstring(resp.text)
 
-    search_res = dom.xpath('//div[@id="search_res"]/table/tr')
+    search_res = dom.xpath('//div[@class="one_result"]')
 
     # return empty array if nothing is found
     if not search_res:
@@ -47,23 +47,16 @@ def response(resp):
 
     # parse results
     for result in search_res:
-        link = result.xpath('.//td[@class="torrent_name"]//a')[0]
+        link = result.xpath('.//div[@class="torrent_name"]//a')[0]
         href = urljoin(url, link.attrib.get('href'))
         title = extract_text(link)
-        content = extract_text(result.xpath('.//pre[@class="snippet"]')[0])
+        content = extract_text(result.xpath('.//div[@class="torrent_excerpt"]')[0])
         content = "<br />".join(content.split("\n"))
 
-        filesize = result.xpath('.//span[@class="attr_val"]/text()')[0].split()[0]
-        filesize_multiplier = result.xpath('.//span[@class="attr_val"]/text()')[0].split()[1]
-        files = result.xpath('.//span[@class="attr_val"]/text()')[1]
-        seed = result.xpath('.//span[@class="attr_val"]/text()')[2]
-
-        # convert seed to int if possible
-        if seed.isdigit():
-            seed = int(seed)
-        else:
-            seed = 0
-
+        filesize = result.xpath('.//span[@class="torrent_size"]/text()')[0].split()[0]
+        filesize_multiplier = result.xpath('.//span[@class="torrent_size"]/text()')[0].split()[1]
+        files = (result.xpath('.//span[@class="torrent_files"]/text()') or [1])[0]
+        seed = 0
         leech = 0
 
         # convert filesize to byte if possible
@@ -75,7 +68,7 @@ def response(resp):
         else:
             files = None
 
-        magnetlink = result.xpath('.//td[@class="ttth"]//a')[0].attrib['href']
+        magnetlink = result.xpath('.//div[@class="torrent_magnet"]//a')[0].attrib['href']
 
         # append result
         results.append({'url': href,
