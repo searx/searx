86ebda6d08cb8fc3f3f46ec6653151010c9f7c2d
frankdelange <yetangitu-f@unternet.org>
2019-04-18 01:51:31 +0200

    devianart: use RSS API, add 'content', 'img_format' and 'source' (these two need PR #1567 to be displayed), 'author' fields, use biggest thumbnail image available
    satisfy pep8's navel-gazing
    
    More pep8 navel-gazing plus remove 'rewrite to use api' as that is just what this change accomplishes
    
    squashed

--- searx/engines/deviantart.py
+++ searx/engines/deviantart.py
@@ -4,32 +4,36 @@
  @website     https://www.deviantart.com/
  @provide-api yes (https://www.deviantart.com/developers/) (RSS)
 
- @using-api   no (TODO, rewrite to api)
- @results     HTML
- @stable      no (HTML can change)
+ @using-api   yes
+ @results     RSS
+ @stable      yes
  @parse       url, title, thumbnail_src, img_src
-
- @todo        rewrite to api
 """
 
-from lxml import html
+from lxml import etree
 import re
+import os.path
 from searx.engines.xpath import extract_text
-from searx.url_utils import urlencode
+from searx.url_utils import urlencode, unquote
 
 # engine dependent config
 categories = ['images']
 paging = True
 time_range_support = True
+number_of_results = 24
 
 # search-url
-base_url = 'https://www.deviantart.com/'
-search_url = base_url + 'browse/all/?offset={offset}&{query}'
+base_url = 'https://backend.deviantart.com/'
+search_url = base_url + 'rss.xml?type=deviation'\
+    '&{query}'\
+    '&{offset}'
 time_range_url = '&order={range}'
 
 time_range_dict = {'day': 11,
                    'week': 14,
                    'month': 15}
+
+strip_tags_re = re.compile(r'<[^>]*?>')
 
 
 # do search-request
@@ -37,7 +41,7 @@ def request(query, params):
     if params['time_range'] and params['time_range'] not in time_range_dict:
         return params
 
-    offset = (params['pageno'] - 1) * 24
+    offset = (params['pageno'] - 1) * number_of_results
 
     params['url'] = search_url.format(offset=offset,
                                       query=urlencode({'q': query}))
@@ -51,34 +55,50 @@ def request(query, params):
 def response(resp):
     results = []
 
-    # return empty array if a redirection code is returned
-    if resp.status_code == 302:
-        return []
+    rss = etree.fromstring(resp.content)
+    ns = rss.nsmap
 
-    dom = html.fromstring(resp.text)
+    for item in rss.xpath('./channel/item'):
 
-    regex = re.compile(r'\/200H\/')
+        # find largest thumbnail
+        thumbnail_area = 0
+        for thumbnail in item.xpath('./media:thumbnail', namespaces=ns):
+            t_width = int(thumbnail.xpath('./@width')[0])
+            t_height = int(thumbnail.xpath('./@height')[0])
+            if (t_width * t_height) > thumbnail_area:
+                thumbnail_area = t_width * t_height
+                thumbnail_src = thumbnail.xpath('./@url')[0]
 
-    # parse results
-    for result in dom.xpath('.//span[@class="thumb wide"]'):
-        link = result.xpath('.//a[@class="torpedo-thumb-link"]')[0]
-        url = link.attrib.get('href')
-        title = extract_text(result.xpath('.//span[@class="title"]'))
-        thumbnail_src = link.xpath('.//img')[0].attrib.get('src')
-        img_src = regex.sub('/', thumbnail_src)
+        # could also used 'guid' which seems to contain a permalink
+        url = item.xpath('./link/text()')[0]
 
-        # http to https, remove domain sharding
-        thumbnail_src = re.sub(r"https?://(th|fc)\d+.", "https://th01.", thumbnail_src)
-        thumbnail_src = re.sub(r"http://", "https://", thumbnail_src)
+        # the choice is between 'title' and 'media:title', they seem to contain the same data
+        # so choose the former
+        title = item.xpath('./title/text()')[0]
 
-        url = re.sub(r"http://(.*)\.deviantart\.com/", "https://\\1.deviantart.com/", url)
+        img_src = item.xpath('./media:content/@url', namespaces=ns)[0]
+
+        # media:content has a 'medium' attribute but it always contains the rather
+        # non-descriptive term 'image', instead extract the extension from the
+        # img_src url instead to get something more descriptive.
+        img_format = "{0} {1}x{2}".format(
+            os.path.splitext(item.xpath('./media:content/@url', namespaces=ns)[0])[1][1:],
+            str(item.xpath('./media:content/@width', namespaces=ns)[0]),
+            str(item.xpath('./media:content/@height', namespaces=ns)[0]))
+
+        content = re.sub(strip_tags_re, '', unquote(item.xpath('./description/text()')[0]))
+        source = item.xpath('./media:copyright/text()', namespaces=ns)[0]
+        author = item.xpath('./media:credit', namespaces=ns)[0].xpath('text()')[0]
 
         # append result
         results.append({'url': url,
                         'title': title,
                         'img_src': img_src,
                         'thumbnail_src': thumbnail_src,
+                        'img_format': img_format,
+                        'source': source,
+                        'content': content,
+                        'author': author,
                         'template': 'images.html'})
 
-    # return results
     return results
--- tests/unit/engines/test_deviantart.py
+++ tests/unit/engines/test_deviantart.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import mock
 from searx.engines import deviantart
 from searx.testing import SearxTestCase
+import lxml
 
 
 class TestDeviantartEngine(SearxTestCase):
@@ -29,67 +30,70 @@ class TestDeviantartEngine(SearxTestCase):
         self.assertRaises(AttributeError, deviantart.response, '')
         self.assertRaises(AttributeError, deviantart.response, '[]')
 
-        response = mock.Mock(text='<html></html>')
+        response = mock.Mock(content='<html></html>')
         self.assertEqual(deviantart.response(response), [])
 
-        response = mock.Mock(status_code=302)
-        self.assertEqual(deviantart.response(response), [])
-
-        html = """
-        <div id="page-1-results" class="page-results results-page-thumb torpedo-container">
-        <span class="thumb wide" href="http://amai911.deviantart.com/art/Horse-195212845"
-        data-super-full-width="900" data-super-full-height="600">
-            <a class="torpedo-thumb-link" href="https://url.of.image">
-                <img data-sigil="torpedo-img" src="https://url.of.thumbnail" />
-            </a>
-        <span class="info"><span class="title-wrap"><span class="title">Title of image</span></span>
-        </div>
+        # correctness test
+        rss = """
+<rss xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom"
+xmlns:creativeCommons="http://backend.userland.com/creativeCommonsRssModule" version="2.0">
+  <channel>
+    <title>DeviantArt: Popular Test</title>
+    <link>https://www.deviantart.com/popular-all-time/?q=test</link>
+    <description>DeviantArt RSS for Popular Test</description>
+    <language>en-us</language>
+    <copyright>Copyright 2019, DeviantArt.com</copyright>
+    <pubDate>Wed, 17 Apr 2019 15:39:48 PDT</pubDate>
+    <generator>DeviantArt.com</generator>
+    <docs>http://blogs.law.harvard.edu/tech/rss</docs>
+    <atom:icon>https://st.deviantart.net/minish/touch-icons/android-192.png</atom:icon>
+    <atom:link type="application/rss+xml" rel="self"
+    href="https://backend.deviantart.com/rss.xml?type=deviation&amp;q=test&amp;order=9"/>
+    <atom:link rel="next"
+    href="https://backend.deviantart.com/rss.xml?type=deviation&amp;q=test&amp;order=9&amp;offset=60"/>
+    <item>
+      <title>Test 1 title</title>
+      <link>https://www.deviantart.com/testauthor/art/Test-1</link>
+      <guid isPermaLink="true">https://www.deviantart.com/testauthor/art/Test-1</guid>
+      <pubDate>Fri, 08 Jun 2012 12:47:04 PDT</pubDate>
+      <media:title type="plain">Test</media:title>
+      <media:keywords/>
+      <media:rating>nonadult</media:rating>
+      <media:credit role="author" scheme="urn:ebu">TestAuthor</media:credit>
+      <media:credit role="author" scheme="urn:ebu">https://a.deviantart.net/avatars/t/e/testauthor.jpg?7</media:credit>
+      <media:copyright url="https://www.deviantart.com/testauthor">Copyright TestAuthor</media:copyright>
+      <media:description type="html">testdescription</media:description>
+      <media:thumbnail url="https://t00.deviantart.net/testthumb_small_1.jpg" height="89" width="150"/>
+      <media:thumbnail url="https://t00.deviantart.net/testthumb_large_1.jpg" height="178" width="300"/>
+      <media:thumbnail url="https://t00.deviantart.net/testthumb_square_1.jpg" height="178" width="178"/>
+      <media:content url="https://orig00.deviantart.net/test_1.jpg" height="476" width="802" medium="image"/>
+      <description>testdescription</description>
+    </item>
+  </channel>
+</rss>
         """
-        response = mock.Mock(text=html)
+        response = mock.Mock(content=rss)
         results = deviantart.response(response)
         self.assertEqual(type(results), list)
         self.assertEqual(len(results), 1)
-        self.assertEqual(results[0]['title'], 'Title of image')
-        self.assertEqual(results[0]['url'], 'https://url.of.image')
-        self.assertNotIn('content', results[0])
-        self.assertEqual(results[0]['thumbnail_src'], 'https://url.of.thumbnail')
+        self.assertEqual(results[0]['title'], 'Test 1 title')
+        self.assertEqual(results[0]['content'], 'testdescription')
+        self.assertEqual(results[0]['author'], 'TestAuthor')
+        self.assertEqual(results[0]['source'], 'Copyright TestAuthor')
+        self.assertEqual(results[0]['img_format'], 'jpg 802x476')
+        self.assertEqual(results[0]['img_src'], 'https://orig00.deviantart.net/test_1.jpg')
+        self.assertEqual(results[0]['thumbnail_src'], 'https://t00.deviantart.net/testthumb_large_1.jpg')
+        self.assertEqual(results[0]['url'], 'https://www.deviantart.com/testauthor/art/Test-1')
 
+        # garbage test
         html = """
-        <span class="tt-fh-tc" style="width: 202px;">
-            <span class="tt-bb" style="width: 202px;">
-            </span>
-            <span class="shadow">
-                <a class="thumb" href="http://url.of.result/2nd.part.of.url"
-                    title="Behoimi BE Animation Test by test-0, Jan 4,
-                    2010 in Digital Art &gt; Animation"> <i></i>
-                    <img width="200" height="200" alt="Test"
-                        src="http://url.of.thumbnail" data-src="http://th08.deviantart.net/test.jpg">
-                </a>
-            </span>
-            <!-- ^TTT -->
-        </span>
-        <span class="details">
-            <a href="http://test-0.deviantart.com/art/Test" class="t"
-                title="Behoimi BE Animation Test by test-0, Jan 4, 2010">
-                <span class="tt-fh-oe">Title of image</span> </a>
-            <small>
-            <span class="category">
-                <span class="age">
-                    5 years ago
-                </span>
-                in <a title="Behoimi BE Animation Test by test-0, Jan 4, 2010"
-                    href="http://www.deviantart.com/browse/all/digitalart/animation/">Animation</a>
-            </span>
-            <div class="commentcount">
-                <a href="http://test-0.deviantart.com/art/Test#comments">
-                <span class="iconcommentsstats"></span>9 Comments</a>
-            </div>
-            <a class="mlt-link" href="http://www.deviantart.com/morelikethis/149167425">
-            <span class="mlt-icon"></span> <span class="mlt-text">More Like This</span> </a>
-        </span>
-        </small> <!-- TTT$ -->
+<HTML><HEAD><TITLE>ERROR: Fluxcapacitor discharged, need 1.21 gigawatts to charge</TITLE></HEAD><BODY>
+<H1>403 ERROR</H1>
+<PRE>
+This is only a test
+</PRE>
+</BODY></HTML>
         """
-        response = mock.Mock(text=html)
+        response = mock.Mock(content=html)
         results = deviantart.response(response)
-        self.assertEqual(type(results), list)
-        self.assertEqual(len(results), 0)
+        self.assertEqual(deviantart.response(response), [])
