275b37cc7c87b562d08576be5268a4f8797b84ea
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-07-31 21:01:24 +0200

    Fix error if the user hasn't set api credentials

--- searx/engines/spotify.py
+++ searx/engines/spotify.py
@@ -39,7 +39,7 @@ def request(query, params):
         'https://accounts.spotify.com/api/token',
         data={'grant_type': 'client_credentials'},
         headers={'Authorization': 'Basic ' + str(base64.b64encode(
-            (api_client_id + ":" + api_client_secret).encode('utf-8')
+            "{}:{}".format(api_client_id, api_client_secret).encode('utf-8')
         ), 'utf-8')}
     )
     j = loads(r.text)
