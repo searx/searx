cdf55c3b2e3618b6017ba95b1d941d504649bf3e
Dalf <alex@al-f.net>
2019-08-01 19:29:16 +0200

    [fix] python2 encoding

--- searx/utils.py
+++ searx/utils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import csv
 import hashlib
 import hmac
@@ -44,8 +45,8 @@ logger = logger.getChild('utils')
 blocked_tags = ('script',
                 'style')
 
-ecma_unescape4_re = re.compile(r'%u([0-9a-fA-F]{4})')
-ecma_unescape2_re = re.compile(r'%([0-9a-fA-F]{2})')
+ecma_unescape4_re = re.compile(r'%u([0-9a-fA-F]{4})', re.UNICODE)
+ecma_unescape2_re = re.compile(r'%([0-9a-fA-F]{2})', re.UNICODE)
 
 useragents = json.loads(open(os.path.dirname(os.path.realpath(__file__))
                              + "/data/useragents.json", 'r', encoding='utf-8').read())
@@ -427,8 +428,9 @@ def ecma_unescape(s):
     https://www.ecma-international.org/ecma-262/6.0/#sec-unescape-string
     https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/unescape
     """
+    # s = unicode(s)
     # "%u5409" becomes "吉"
-    s = ecma_unescape4_re.sub(lambda e: chr(int(e.group(1), 16)), s)
+    s = ecma_unescape4_re.sub(lambda e: unichr(int(e.group(1), 16)), s)
     # "%20" becomes " ", "%F3" becomes "ó"
-    s = ecma_unescape2_re.sub(lambda e: chr(int(e.group(1), 16)), s)
+    s = ecma_unescape2_re.sub(lambda e: unichr(int(e.group(1), 16)), s)
     return s
