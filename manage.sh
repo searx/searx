e3f1a76decf3442d8d0c78698ac00424f64eda23
rachmadaniHaryono <foreturiga@gmail.com>
2019-08-09 17:37:03 +0800

    new: test: info line

--- manage.sh
+++ manage.sh
@@ -76,6 +76,7 @@ pep8_check() {
     #  E402 module level import not at top of file
     #  W503 line break before binary operator
     pep8 --exclude=searx/static --max-line-length=120 --ignore "E402,W503" "$SEARX_DIR" "$BASE_DIR/tests"
+    echo '[!] Running flake8 check'
     # ignored rules
     # E117 over-indented
     # E211 whitespace before '('
