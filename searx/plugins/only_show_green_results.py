95226a72b6b728759ad4a0ce2d03234ab581c147
Chris Adams <chris@productscience.co.uk>
2020-03-03 16:15:31 +0100

    Try the SPDX-License-Identifier as requested

--- searx/plugins/only_show_green_results.py
+++ searx/plugins/only_show_green_results.py
@@ -1,18 +1,5 @@
 '''
-searx is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-searx is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with searx. If not, see < http://www.gnu.org/licenses/ >.
-
-(C) 2015 by Adam Tauber, <asciimoo@gmail.com>
+ SPDX-License-Identifier: AGPL-3.0-or-later
 '''
 
 from flask_babel import gettext
