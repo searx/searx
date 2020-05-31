d7d89369cb3f9d33855749f13c410fb444a81104
Noémi Ványi <sitbackandwait@gmail.com>
2020-05-31 22:36:09 +0200

    add disable all and allow all engines button to Oscar preferences

--- searx/static/themes/oscar/js/searx.js
+++ searx/static/themes/oscar/js/searx.js
@@ -357,3 +357,13 @@ $(document).ready(function(){
         $( this ).off( event );
     });
 });
+;$(document).ready(function(){
+    $("#allow-all-engines").click(function() {
+        $(".onoffswitch-checkbox").each(function() { this.checked = false;});
+    });
+
+    $("#disable-all-engines").click(function() {
+        $(".onoffswitch-checkbox").each(function() { this.checked = true;});
+    });
+});
+
