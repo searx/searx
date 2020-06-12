6ce70843e5357648bb33a01c2642ddd8b181592d
root <root@v220200573057117970.hotsrv.de>
2020-06-12 02:44:01 +0200

    Respect "Results on new tabs" setting on oscar theme

--- searx/static/plugins/js/open_results_on_new_tab.js
+++ searx/static/plugins/js/open_results_on_new_tab.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    $('.result_header > a').attr('target', '_blank');
+    // Only add target=_blank when cookie is present (setting) and set to 1
+    let matchingCookies = document.cookie.split(' ').filter(e => e.startsWith('results_on_new_tab='));
+    if(matchingCookies.length == 1) {
+      let resultsOnNewTab = parseInt(matchingCookies[0].split('=')[1]);
+
+      if(resultsOnNewTab)
+        $('.result_header > a').attr('target', '_blank');
+    }
 });
