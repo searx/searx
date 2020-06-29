281a486bdaac6067eeb947531550beab78321e4e
Lukas van den Berk <lukasvdberk@gmail.com>
2020-06-29 15:03:32 +0200

    Refactored bangs unit tests

--- tests/unit/test_plugins.py
+++ tests/unit/test_plugins.py
@@ -96,44 +96,17 @@ class SelfBangTest(SearxTestCase):
         self.assertTrue(len(store.plugins) == 1)
 
         # Valid Bang redirect test
-        search_valid = get_search_mock(query='!yt never gonna give you up')
+        search_valid = get_search_mock(query='!!yt never gonna give you up')
 
-        custom_result_plugin = store.call(
-            store.plugins, 'custom_results', search_valid.search_query, request)
-        if custom_result_plugin is not None:
-            # 302 is for a redirect.
-            self.assertTrue(custom_result_plugin.status_code == 302)
-            # For checking what the redirect URL was.
-            self.assertTrue(custom_result_plugin.location == "https://www.youtube.com/results?search_query=never"
-                                                             "%20gonna%20give%20you%20up%20")
+        store.call(store.plugins, 'pre_search', search_valid, request)
+
+        # For checking what the redirect URL was.
+        self.assertTrue(search_valid.result_container.redirect_url ==
+                        "https://www.youtube.com/results?search_query=never%20gonna%20give%20you%20up%20")
 
         # Invalid Bang redirect test
         invalid_search = get_search_mock(query='youtube never gonna give you up')
+        store.call(store.plugins, 'pre_search', invalid_search, request)
 
-        custom_result_plugin = store.call(
-            store.plugins, 'pre_search', invalid_search.search_query, request)
-        if custom_result_plugin is not None:
-            # 302 is for a redirect.
-            self.assertFalse(custom_result_plugin.status_code == 302)
-
-        with app.app_context():
-            # Check if it gets a list of all the bangs
-            bangs_search = get_search_mock(query='!bangs')
-
-            custom_result_plugin = store.call(
-                store.plugins, 'pre_search', bangs_search.search_query, request)
-            if custom_result_plugin is not None:
-                # The !bangs operator should return application/json
-                self.assertTrue(custom_result_plugin.headers["Content-Type"] == "application/json")
-
-            # Check if it shows the help page.
-            help_search = get_search_mock(query='!help')
-
-            custom_result_plugin = store.call(
-                store.plugins, 'pre_search', help_search.search_query, request)
-            if custom_result_plugin is not None:
-                # Check if a text on the help page is shown on the result.
-                self.assertIn(
-                    '<h2>To use any of the help operators enter them query field on the home page.</h2>',
-                    custom_result_plugin
-                )
+        self.assertFalse(invalid_search.result_container.redirect_url ==
+                         "https://www.youtube.com/results?search_query=never%20gonna%20give%20you%20up%20")
