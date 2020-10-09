e158eeee4bf69b973659f68a799d5c005aa1c0ae
Noémi Ványi <sitbackandwait@gmail.com>
2020-10-09 17:34:26 +0200

    Propagate error messages from YouTube API

--- searx/engines/youtube_api.py
+++ searx/engines/youtube_api.py
@@ -47,6 +47,9 @@ def response(resp):
 
     search_results = loads(resp.text)
 
+    if 'error' in search_results and 'message' in search_results['error']:
+        raise Exception(search_results['error']['message'])
+
     # return empty array if there are no results
     if 'items' not in search_results:
         return []
