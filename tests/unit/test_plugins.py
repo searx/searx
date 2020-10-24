35577051e218dad81a40a12ec97d152f74c0a8b3
Venca24 <Vaclav.Zouzalik@seznam.cz>
2020-10-24 10:25:33 +0200

    [fix] hash plugin test

--- tests/unit/test_plugins.py
+++ tests/unit/test_plugins.py
@@ -97,42 +97,43 @@ class HashPluginTest(SearxTestCase):
         request.headers.getlist.return_value = []
 
         # MD5
-        search = get_search_mock(query=b'md5 test', pageno=1)
+        search = get_search_mock(query='md5 test', pageno=1)
         store.call(store.plugins, 'post_search', request, search)
-        self.assertTrue('md5 hash function: 098f6bcd4621d373cade4e832627b4f6' in search.result_container.answers)
+        self.assertTrue('md5 hash digest: 098f6bcd4621d373cade4e832627b4f6'
+                        in search.result_container.answers['hash']['answer'])
 
         search = get_search_mock(query=b'md5 test', pageno=2)
         store.call(store.plugins, 'post_search', request, search)
-        self.assertFalse('md5 hash function: 098f6bcd4621d373cade4e832627b4f6' in search.result_container.answers)
+        self.assertFalse('hash' in search.result_container.answers)
 
         # SHA1
-        search = get_search_mock(query=b'sha1 test', pageno=1)
+        search = get_search_mock(query='sha1 test', pageno=1)
         store.call(store.plugins, 'post_search', request, search)
-        self.assertTrue('sha1 hash function: a94a8fe5ccb19ba61c4c0873d391e9879'
-                        '82fbbd3' in search.result_container.answers)
+        self.assertTrue('sha1 hash digest: a94a8fe5ccb19ba61c4c0873d391e9879'
+                        '82fbbd3' in search.result_container.answers['hash']['answer'])
 
         # SHA224
-        search = get_search_mock(query=b'sha224 test', pageno=1)
+        search = get_search_mock(query='sha224 test', pageno=1)
         store.call(store.plugins, 'post_search', request, search)
-        self.assertTrue('sha224 hash function: 90a3ed9e32b2aaf4c61c410eb9254261'
-                        '19e1a9dc53d4286ade99a809' in search.result_container.answers)
+        self.assertTrue('sha224 hash digest: 90a3ed9e32b2aaf4c61c410eb9254261'
+                        '19e1a9dc53d4286ade99a809' in search.result_container.answers['hash']['answer'])
 
         # SHA256
-        search = get_search_mock(query=b'sha256 test', pageno=1)
+        search = get_search_mock(query='sha256 test', pageno=1)
         store.call(store.plugins, 'post_search', request, search)
-        self.assertTrue('sha256 hash function: 9f86d081884c7d659a2feaa0c55ad015a'
-                        '3bf4f1b2b0b822cd15d6c15b0f00a08' in search.result_container.answers)
+        self.assertTrue('sha256 hash digest: 9f86d081884c7d659a2feaa0c55ad015a'
+                        '3bf4f1b2b0b822cd15d6c15b0f00a08' in search.result_container.answers['hash']['answer'])
 
         # SHA384
-        search = get_search_mock(query=b'sha384 test', pageno=1)
+        search = get_search_mock(query='sha384 test', pageno=1)
         store.call(store.plugins, 'post_search', request, search)
-        self.assertTrue('sha384 hash function: 768412320f7b0aa5812fce428dc4706b3c'
+        self.assertTrue('sha384 hash digest: 768412320f7b0aa5812fce428dc4706b3c'
                         'ae50e02a64caa16a782249bfe8efc4b7ef1ccb126255d196047dfedf1'
-                        '7a0a9' in search.result_container.answers)
+                        '7a0a9' in search.result_container.answers['hash']['answer'])
 
         # SHA512
-        search = get_search_mock(query=b'sha512 test', pageno=1)
+        search = get_search_mock(query='sha512 test', pageno=1)
         store.call(store.plugins, 'post_search', request, search)
-        self.assertTrue('sha512 hash function: ee26b0dd4af7e749aa1a8ee3c10ae9923f6'
+        self.assertTrue('sha512 hash digest: ee26b0dd4af7e749aa1a8ee3c10ae9923f6'
                         '18980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5'
-                        'fa9ad8e6f57f50028a8ff' in search.result_container.answers)
+                        'fa9ad8e6f57f50028a8ff' in search.result_container.answers['hash']['answer'])
