6e4b56a5668eddbf55ff7ccbc85e526b960d3323
rachmadaniHaryono <foreturiga@gmail.com>
2020-10-30 21:33:50 +0800

    new: test: standalone_searx

--- tests/unit/test_standalone_searx.py
+++ tests/unit/test_standalone_searx.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """Test utils/standalone_searx.py"""
+import datetime
 import importlib.util
 import sys
 
-from mock import patch
+from mock import Mock, patch
+from nose2.tools import params
 
 from searx.testing import SearxTestCase
 
@@ -42,8 +44,75 @@ class StandaloneSearx(SearxTestCase):
         res2 = sas.parse_argument(args[1:])
         self.assertEqual(exp_dict, vars(res2))
 
-    def test_main_basic_args(self):
-        """test basic args for main func."""
+    def test_to_dict(self):
+        """test to_dict."""
         sas = get_standalone_searx_module()
-        res = sas.main(sas.parse_argument(['red box']))
-        self.assertTrue(res)
+        self.assertEqual(
+            sas.to_dict(
+                sas.get_search_query(sas.parse_argument(['red box']))),
+            {
+                'search': {
+                    'q': 'red box', 'pageno': 1, 'lang': 'all',
+                    'safesearch': 0, 'timerange': None
+                },
+                'results': [], 'infoboxes': [], 'suggestions': [],
+                'answers': [], 'paging': False, 'results_number': 0
+            }
+        )
+
+    def test_to_dict_with_mock(self):
+        """test to dict."""
+        sas = get_standalone_searx_module()
+        with patch.object(sas.searx.search, 'Search') as mock_s:
+            m_search = mock_s().search()
+            m_sq = Mock()
+            self.assertEqual(
+                sas.to_dict(m_sq),
+                {
+                    'answers': [],
+                    'infoboxes': m_search.infoboxes,
+                    'paging': m_search.paging,
+                    'results': m_search.get_ordered_results(),
+                    'results_number': m_search.results_number(),
+                    'search': {
+                        'lang': m_sq.lang,
+                        'pageno': m_sq.pageno,
+                        'q': m_sq.query,
+                        'safesearch': m_sq.safesearch,
+                        'timerange': m_sq.time_range,
+                    },
+                    'suggestions': []
+                }
+            )
+
+    def test_get_search_query(self):
+        """test get_search_query."""
+        sas = get_standalone_searx_module()
+        args = sas.parse_argument(['rain', ])
+        search_q = sas.get_search_query(args)
+        self.assertTrue(search_q)
+        self.assertEqual(str(search_q), 'rain;[]')
+
+    def test_no_parsed_url(self):
+        """test no_parsed_url func"""
+        sas = get_standalone_searx_module()
+        self.assertEqual(
+            sas.no_parsed_url([{'parsed_url': 'http://example.com'}]),
+            [{}]
+        )
+
+    @params(
+        (datetime.datetime(2020, 1, 1), '2020-01-01T00:00:00'),
+        ('a'.encode('utf8'), 'a'),
+        (set([1]), [1])
+    )
+    def test_json_serial(self, arg, exp_res):
+        """test json_serial func"""
+        sas = get_standalone_searx_module()
+        self.assertEqual(sas.json_serial(arg), exp_res)
+
+    def test_json_serial_error(self):
+        """test error on json_serial."""
+        sas = get_standalone_searx_module()
+        with self.assertRaises(TypeError):
+            sas.json_serial('a')
