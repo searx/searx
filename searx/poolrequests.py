1342f3b0d1a22ecbe6308ea082d4db4e49e61b31
Anton Nesterov <anton@nesterov.cc>
2020-11-21 18:10:30 +0000

    feat(engine): support DICT protocol, add dict engine

--- searx/poolrequests.py
+++ searx/poolrequests.py
@@ -2,8 +2,11 @@ import sys
 from time import time
 from itertools import cycle
 from threading import RLock, local
+from urllib.parse import urlparse
+from dictionary_client import DictionaryClient
 
 import requests
+import socks
 
 from searx import settings
 from searx import logger
@@ -79,6 +82,12 @@ if settings['outgoing'].get('source_ips'):
 else:
     http_adapters = cycle((HTTPAdapterWithConnParams(pool_connections=connect, pool_maxsize=maxsize), ))
     https_adapters = cycle((HTTPAdapterWithConnParams(pool_connections=connect, pool_maxsize=maxsize), ))
+
+proxyTypes = dict(
+    socks5=socks.SOCKS5,
+    socks4=socks.SOCKS4,
+    http=socks.HTTP
+)
 
 
 class SessionSinglePool(requests.Session):
@@ -182,3 +191,60 @@ def patch(url, data=None, **kwargs):
 
 def delete(url, **kwargs):
     return request('delete', url, **kwargs)
+
+
+def dict_request(method, query, **kwargs):
+    time_before_request = time()
+
+    # timeout
+    if 'timeout' in kwargs:
+        timeout = kwargs['timeout']
+    else:
+        timeout = getattr(threadLocal, 'timeout', None)
+        if timeout is not None:
+            kwargs['timeout'] = timeout
+
+    dc = DictionaryClient(host=kwargs['host'], port=kwargs['port'], sock_class=socks.socksocket)
+
+    # get proxy config
+    proxies = kwargs.get('proxies')
+    if proxies is None:
+        proxies = settings['outgoing'].get('proxies')
+    if proxies is not None and 'dict' in proxies:
+        parsedProxy = urlparse(proxies['dict'])
+        proxyType = proxyTypes.get(parsedProxy.scheme, None)
+        dc.sock.setProxy(proxyType, parsedProxy.hostname, parsedProxy.port)
+
+    # map available dict methods
+    dict_methods = dict(
+        define=dc.define,
+        match=dc.match
+    )
+    dict_response = dict_methods.get(method)(query, db=kwargs['db'])
+    response = dict(
+        content=dict_response.content,
+        databases=dc.databases
+    )
+
+    # request finished here
+    time_after_request = time()
+
+    # is there a timeout for this engine ?
+    if timeout is not None:
+        timeout_overhead = 0.2  # seconds
+        # start_time = when the user request started
+        start_time = getattr(threadLocal, 'start_time', time_before_request)
+        search_duration = time_after_request - start_time
+        if search_duration > timeout + timeout_overhead:
+            raise requests.exceptions.Timeout(response=response)
+
+    dc.disconnect()
+
+    if hasattr(threadLocal, 'total_time'):
+        threadLocal.total_time += time_after_request - time_before_request
+
+    return response
+
+
+def dict_define(query, **kwargs):
+    return dict_request('define', query, **kwargs)
