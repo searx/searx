6e2872f43625aba71eba019e16f7fbd74743f590
Alexandre Flament <alex@al-f.net>
2021-01-05 11:22:48 +0100

    [enh] add searx.shared
    shared dictionary between the workers (UWSGI or werkzeug)
    scheduler: run a task once every x seconds (UWSGI or werkzeug)

---
+++ searx/shared/shared_abstract.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class SharedDict:
+
+    def get_int(self, key):
+        pass
+
+    def set_int(self, key, value):
+        pass
+
+    def get_str(self, key):
+        pass
+
+    def set_str(self, key, value):
+        pass
