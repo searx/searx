ac24978633e1f79f15dfde39d970f2f6dbc6170e
Alexandre Flament <alex@al-f.net>
2021-01-20 16:06:22 +0100

    [mod] processors: move suspended_time from searx.engine.engines to searx.search.processors.abstract
    Definition & usage of suspend_time in one file instead of two

--- searx/search/processors/abstract.py
+++ searx/search/processors/abstract.py
@@ -1,7 +1,11 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import threading
 from abc import abstractmethod, ABC
+from time import time
+
 from searx import logger
+from searx.engines import settings
 
 
 logger = logger.getChild('searx.search.processor')
@@ -12,8 +16,31 @@ class EngineProcessor(ABC):
     def __init__(self, engine, engine_name):
         self.engine = engine
         self.engine_name = engine_name
+        self.suspend_end_time = 0
+        self.continuous_errors = 0
+
+    def suspend(self, suspended_time):
+        with threading.RLock():
+            # update continuous_errors / suspend_end_time
+            self.continuous_errors += 1
+            if suspended_time is None:
+                suspended_time = min(settings['search']['max_ban_time_on_fail'],
+                                     self.continuous_errors * settings['search']['ban_time_on_fail'])
+            self.suspend_end_time = time() + suspended_time
+            logger.debug('Suspend engine for %i seconds', suspended_time)
+
+    def sucessful_request(self):
+        with threading.RLock():
+            # reset the suspend variables
+            self.continuous_errors = 0
+            self.suspend_end_time = 0
 
     def get_params(self, search_query, engine_category):
+        # skip suspended engines
+        if self.suspend_end_time >= time():
+            logger.debug('Engine currently suspended: %s', self.engine_name)
+            return None
+
         # if paging is not supported, skip
         if search_query.pageno > 1 and not self.engine.paging:
             return None
