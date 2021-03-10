bdb41bea7b4db6e97611a8303e4880e580334a74
Alexandre Flament <alex@al-f.net>
2021-03-10 14:34:33 +0100

    [mod] theme: remove require-2.1.15.min.js
    See https://github.com/requirejs/requirejs/issues/1816
    
    requirejs loads one file: leaflet.
    
    This commit:
    * removes requirejs
    * load leaflet using <script src...> HTML tag in searx/templates/oscar/base.html

--- searx/static/themes/oscar/js/searx.js
+++ searx/static/themes/oscar/js/searx.js
@@ -12,29 +12,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with searx. If not, see < http://www.gnu.org/licenses/ >.
  *
- * (C) 2014 by Thomas Pointhuber, <thomas.pointhuber@gmx.at>
- */
-
-requirejs.config({
-    baseUrl: './static/themes/oscar/js',
-    paths: {
-        app: '../app'
-    }
-});
-;/**
- * searx is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * searx is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with searx. If not, see < http://www.gnu.org/licenses/ >.
- *
  * (C) 2019 by Alexandre Flament
  */
 window.searx = (function(d) {
@@ -331,59 +308,54 @@ $(document).ready(function(){
         var map_boundingbox = $(this).data('map-boundingbox');
         var map_geojson = $(this).data('map-geojson');
 
-        require(['leaflet-0.7.3.min'], function(leaflet) {
-            if(map_boundingbox) {
-                southWest = L.latLng(map_boundingbox[0], map_boundingbox[2]);
-                northEast = L.latLng(map_boundingbox[1], map_boundingbox[3]);
-                map_bounds = L.latLngBounds(southWest, northEast);
-            }
+        if(map_boundingbox) {
+            southWest = L.latLng(map_boundingbox[0], map_boundingbox[2]);
+            northEast = L.latLng(map_boundingbox[1], map_boundingbox[3]);
+            map_bounds = L.latLngBounds(southWest, northEast);
+        }
 
-            // TODO hack
-            // change default imagePath
-            L.Icon.Default.imagePath =  "./static/themes/oscar/img/map";
+        // change default imagePath
+        L.Icon.Default.imagePath =  "./static/themes/oscar/img/map";
 
-            // init map
-            var map = L.map(leaflet_target);
+        // init map
+        var map = L.map(leaflet_target);
 
-            // create the tile layer with correct attribution
-            var osmMapnikUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var osmMapnikAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-            var osmMapnik = new L.TileLayer(osmMapnikUrl, {minZoom: 1, maxZoom: 19, attribution: osmMapnikAttrib});
+        // create the tile layer with correct attribution
+        var osmMapnikUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmMapnikAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+        var osmMapnik = new L.TileLayer(osmMapnikUrl, {minZoom: 1, maxZoom: 19, attribution: osmMapnikAttrib});
 
-            var osmWikimediaUrl='https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
-            var osmWikimediaAttrib = 'Wikimedia maps beta | Maps data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-            var osmWikimedia = new L.TileLayer(osmWikimediaUrl, {minZoom: 1, maxZoom: 19, attribution: osmWikimediaAttrib});
+        var osmWikimediaUrl='https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
+        var osmWikimediaAttrib = 'Wikimedia maps beta | Maps data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+        var osmWikimedia = new L.TileLayer(osmWikimediaUrl, {minZoom: 1, maxZoom: 19, attribution: osmWikimediaAttrib});
 
-            // init map view
+        // init map view
+        setTimeout(function() {
             if(map_bounds) {
-                // TODO hack: https://github.com/Leaflet/Leaflet/issues/2021
-                setTimeout(function () {
-                    map.fitBounds(map_bounds, {
-                        maxZoom:17
-                    });
-                }, 0);
+                map.fitBounds(map_bounds, {
+                    maxZoom:17
+                });
             } else if (map_lon && map_lat) {
                 if(map_zoom)
                     map.setView(new L.LatLng(map_lat, map_lon),map_zoom);
                 else
                     map.setView(new L.LatLng(map_lat, map_lon),8);
-            }
+            }    
+        }, 0);
 
-                map.addLayer(osmMapnik);
+        map.addLayer(osmMapnik);
 
-                var baseLayers = {
-             "OSM Mapnik": osmMapnik/*,
-             "OSM Wikimedia": osmWikimedia*/
-            };
+        var baseLayers = {
+            "OSM Mapnik": osmMapnik/*,
+            "OSM Wikimedia": osmWikimedia*/
+        };
 
-            L.control.layers(baseLayers).addTo(map);
+        L.control.layers(baseLayers).addTo(map);
 
-
-            if(map_geojson)
-                L.geoJson(map_geojson).addTo(map);
-            /*else if(map_bounds)
-                L.rectangle(map_bounds, {color: "#ff7800", weight: 3, fill:false}).addTo(map);*/
-        });
+        if(map_geojson)
+            L.geoJson(map_geojson).addTo(map);
+        /*else if(map_bounds)
+            L.rectangle(map_bounds, {color: "#ff7800", weight: 3, fill:false}).addTo(map);*/
 
         // this event occour only once per element
         $( this ).off( event );
--- searx/static/themes/oscar/js/searx_src/leaflet_map.js
+++ searx/static/themes/oscar/js/searx_src/leaflet_map.js
@@ -107,59 +107,54 @@ $(document).ready(function(){
         var map_boundingbox = $(this).data('map-boundingbox');
         var map_geojson = $(this).data('map-geojson');
 
-        require(['leaflet-0.7.3.min'], function(leaflet) {
-            if(map_boundingbox) {
-                southWest = L.latLng(map_boundingbox[0], map_boundingbox[2]);
-                northEast = L.latLng(map_boundingbox[1], map_boundingbox[3]);
-                map_bounds = L.latLngBounds(southWest, northEast);
-            }
+        if(map_boundingbox) {
+            southWest = L.latLng(map_boundingbox[0], map_boundingbox[2]);
+            northEast = L.latLng(map_boundingbox[1], map_boundingbox[3]);
+            map_bounds = L.latLngBounds(southWest, northEast);
+        }
 
-            // TODO hack
-            // change default imagePath
-            L.Icon.Default.imagePath =  "./static/themes/oscar/img/map";
+        // change default imagePath
+        L.Icon.Default.imagePath =  "./static/themes/oscar/img/map";
 
-            // init map
-            var map = L.map(leaflet_target);
+        // init map
+        var map = L.map(leaflet_target);
 
-            // create the tile layer with correct attribution
-            var osmMapnikUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            var osmMapnikAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-            var osmMapnik = new L.TileLayer(osmMapnikUrl, {minZoom: 1, maxZoom: 19, attribution: osmMapnikAttrib});
+        // create the tile layer with correct attribution
+        var osmMapnikUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmMapnikAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+        var osmMapnik = new L.TileLayer(osmMapnikUrl, {minZoom: 1, maxZoom: 19, attribution: osmMapnikAttrib});
 
-            var osmWikimediaUrl='https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
-            var osmWikimediaAttrib = 'Wikimedia maps beta | Maps data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-            var osmWikimedia = new L.TileLayer(osmWikimediaUrl, {minZoom: 1, maxZoom: 19, attribution: osmWikimediaAttrib});
+        var osmWikimediaUrl='https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
+        var osmWikimediaAttrib = 'Wikimedia maps beta | Maps data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+        var osmWikimedia = new L.TileLayer(osmWikimediaUrl, {minZoom: 1, maxZoom: 19, attribution: osmWikimediaAttrib});
 
-            // init map view
+        // init map view
+        setTimeout(function() {
             if(map_bounds) {
-                // TODO hack: https://github.com/Leaflet/Leaflet/issues/2021
-                setTimeout(function () {
-                    map.fitBounds(map_bounds, {
-                        maxZoom:17
-                    });
-                }, 0);
+                map.fitBounds(map_bounds, {
+                    maxZoom:17
+                });
             } else if (map_lon && map_lat) {
                 if(map_zoom)
                     map.setView(new L.LatLng(map_lat, map_lon),map_zoom);
                 else
                     map.setView(new L.LatLng(map_lat, map_lon),8);
-            }
+            }    
+        }, 0);
 
-                map.addLayer(osmMapnik);
+        map.addLayer(osmMapnik);
 
-                var baseLayers = {
-             "OSM Mapnik": osmMapnik/*,
-             "OSM Wikimedia": osmWikimedia*/
-            };
+        var baseLayers = {
+            "OSM Mapnik": osmMapnik/*,
+            "OSM Wikimedia": osmWikimedia*/
+        };
 
-            L.control.layers(baseLayers).addTo(map);
+        L.control.layers(baseLayers).addTo(map);
 
-
-            if(map_geojson)
-                L.geoJson(map_geojson).addTo(map);
-            /*else if(map_bounds)
-                L.rectangle(map_bounds, {color: "#ff7800", weight: 3, fill:false}).addTo(map);*/
-        });
+        if(map_geojson)
+            L.geoJson(map_geojson).addTo(map);
+        /*else if(map_bounds)
+            L.rectangle(map_bounds, {color: "#ff7800", weight: 3, fill:false}).addTo(map);*/
 
         // this event occour only once per element
         $( this ).off( event );
