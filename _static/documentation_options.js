4d1aa800be2026100c545f792ab6027fa4a1c055
asciimoo <asciimoo@gmail.com>
2021-03-27 19:39:04 +0000

    Deploying to gh-pages from  @ ae122ea943f77600fd97556503c483dcd92e1e63 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.18.0',
+    VERSION: '1.0.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
