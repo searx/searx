b3704f58fa75b1b6cf2557a10749544329a77699
Plague Doctor <plague@privacyrequired.com>
2021-04-07 08:48:26 +1000

    Code simplification. Fixes.

--- searx/engines/wordnik.py
+++ searx/engines/wordnik.py
@@ -1,12 +1,14 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-"""
- Wordnik (general)
+"""Wordnik (general)
+
 """
 
-import re
 from lxml.html import fromstring
+from searx import logger
 from searx.utils import extract_text
 from searx.raise_for_httperror import raise_for_httperror
+
+logger = logger.getChild('Wordnik engine')
 
 # about
 about = {
@@ -21,106 +23,55 @@ about = {
 categories = ['general']
 paging = False
 
-word_xpath = '//*[@id="headword"]/text()'
-from_xpath = '//*[@id="define"]/div/h3[1]'
-definitions_xpath = '//*[@id="define"]/div/ul[1]'
-misspelling_xpath = '//*[@id="define"]/div/ul/li/xref'
-
 URL = 'https://www.wordnik.com'
 SEARCH_URL = URL + '/words/{query}'
 
 
 def request(query, params):
     params['url'] = SEARCH_URL.format(query=query)
+    logger.debug(f"query_url --> {params['url']}")
     return params
 
 
 def response(resp):
-    try:
-        dom = fromstring(resp.text)
-    except Exception:
-        raise_for_httperror(resp)
-        return []
-
     results = []
-    word_defs = []
-    word_defs_ib = []
-    urls = []
-    lines = ""
-    lines_ib = "<div>"
 
-    word = extract_text(dom.xpath(word_xpath))
-    definition_from = extract_text(dom.xpath(from_xpath))
-    definitions = dom.xpath(definitions_xpath)
+    raise_for_httperror(resp)
+    dom = fromstring(resp.text)
+    word = extract_text(dom.xpath('//*[@id="headword"]/text()'))
 
-    if len(definitions) == 0:
-        return []
+    definitions = []
+    for src in dom.xpath('//*[@id="define"]//h3[@class="source"]'):
+        src_text = extract_text(src).strip()
+        if src_text.startswith('from '):
+            src_text = src_text[5:]
 
-    for definition in definitions:
-        for i in range(len(definition)):
-            definition_extra_ib = ""
-            definition_extra = ""
+        src_defs = []
+        for def_item in src.xpath('following-sibling::ul[1]/li'):
+            def_abbr = extract_text(def_item.xpath('.//abbr')).strip()
+            def_text = extract_text(def_item).strip()
+            if def_abbr:
+                def_text = def_text[len(def_abbr):].strip()
+            src_defs.append((def_abbr, def_text))
 
-            part_of_speech_xpath = f"./li[{i+1}]/abbr"
-            definition_extra_xpath = f"./li[{i+1}]/i"
-            definition_text_xpath = f"./li[{i+1}]"
+        definitions.append((src_text, src_defs))
 
-            part_of_speech = extract_text(definition.xpath(part_of_speech_xpath))
-            definition_text = extract_text(definition.xpath(definition_text_xpath))
-            definition_extra = extract_text(definition.xpath(definition_extra_xpath))
+    if not definitions:
+        return results
 
-            if 'misspelling' in definition_text.lower():
-                urls.append({
-                    'title': extract_text(definition.xpath(misspelling_xpath)),
-                    'url': f"{URL}/words/{extract_text(definition.xpath(misspelling_xpath))}"})
-
-            definition_text = _lreplace(part_of_speech, '', definition_text.strip())
-            definition_text = _lreplace(definition_extra, '', definition_text.strip())
-
-            if definition_extra is not None and len(definition_extra) > 0:
-                definition_extra_ib = f"<em>({definition_extra})</em>"
-                definition_extra = f"({definition_extra})"
-
-            word_defs_ib.append([
-                i + 1,
-                f"<em><u>{part_of_speech}</u></em>",
-                definition_extra_ib,
-                f"{definition_text}<br><br>"])
-
-            word_defs.append([
-                i + 1,
-                part_of_speech,
-                definition_extra,
-                definition_text])
-
-    for word_def_ib in word_defs_ib:
-        lines_ib += f"{word_def_ib[0]}. \
-                &nbsp; {word_def_ib[1]} \
-                {word_def_ib[2]} \
-                &nbsp; :: &nbsp; {word_def_ib[3]}"
-
-    for word_def in word_defs:
-        lines += f"{word_def[0]}. \
-                {word_def[1]} \
-                {word_def[2]} \
-                :: {word_def[3]} | "
-
-    lines_ib += f"<small>{definition_from}</small></div>"
-
-    urls.append({'title': word, 'url': f"{URL}/words/{word}"})
+    infobox = ''
+    for src_text, src_defs in definitions:
+        infobox += f"<small>{src_text}</small>"
+        infobox += "<ul>"
+        for def_abbr, def_text in src_defs:
+            if def_abbr:
+                def_abbr += ": "
+            infobox += f"<li><i>{def_abbr}</i> {def_text}</li>"
+        infobox += "</ul>"
 
     results.append({
         'infobox': word,
-        'content': lines_ib,
-        'urls': urls})
-
-    results.append({
-        'title': word,
-        'content': lines[:-2],
-        'url': urls[0]['url']})
+        'content': infobox,
+    })
 
     return results
-
-
-def _lreplace(pattern, sub, string):
-    return re.sub('^%s' % pattern, sub, string, 1)
