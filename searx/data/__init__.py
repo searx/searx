c697e350b5b78f684f36f15701afb9000ce116fc
Markus Heiser <markus.heiser@darmarit.de>
2021-06-06 08:13:50 +0200

    [pylint] searx/data/__init__.py
    BTW: add doc strings and moved __all__ to the top [1]
    
    [1] https://www.python.org/dev/peps/pep-0008/#module-level-dunder-names
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/data/__init__.py
+++ searx/data/__init__.py
@@ -1,8 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+# lint: pylint
+"""This module holds the *data* created by::
 
-import json
-from pathlib import Path
+  make data.all
 
+"""
 
 __all__ = [
     'ENGINES_LANGUAGES',
@@ -14,23 +16,32 @@ __all__ = [
     'OSM_KEYS_TAGS',
     'ahmia_blacklist_loader',
 ]
+
+import json
+from pathlib import Path
+
 data_dir = Path(__file__).parent
 
-
-def load(filename):
-    with open(data_dir / filename, encoding='utf-8') as fd:
-        return json.load(fd)
-
+def _load(filename):
+    with open(data_dir / filename, encoding='utf-8') as f:
+        return json.load(f)
 
 def ahmia_blacklist_loader():
-    with open(str(data_dir / 'ahmia_blacklist.txt'), encoding='utf-8') as fd:
-        return fd.read().split()
+    """Load data from `ahmia_blacklist.txt` and return a list of MD5 values of onion
+    names.  The MD5 values are fetched by::
 
+      searx_extra/update/update_ahmia_blacklist.py
 
-ENGINES_LANGUAGES = load('engines_languages.json')
-CURRENCIES = load('currencies.json')
-USER_AGENTS = load('useragents.json')
-EXTERNAL_URLS = load('external_urls.json')
-WIKIDATA_UNITS = load('wikidata_units.json')
-EXTERNAL_BANGS = load('external_bangs.json')
-OSM_KEYS_TAGS = load('osm_keys_tags.json')
+    This function is used by :py:mod:`searx.plugins.ahmia_filter`.
+
+    """
+    with open(str(data_dir / 'ahmia_blacklist.txt'), encoding='utf-8') as f:
+        return f.read().split()
+
+ENGINES_LANGUAGES = _load('engines_languages.json')
+CURRENCIES = _load('currencies.json')
+USER_AGENTS = _load('useragents.json')
+EXTERNAL_URLS = _load('external_urls.json')
+WIKIDATA_UNITS = _load('wikidata_units.json')
+EXTERNAL_BANGS = _load('external_bangs.json')
+OSM_KEYS_TAGS = _load('osm_keys_tags.json')
