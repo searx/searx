3bc4077f3302bad701d6ab9316eeca7c4f8cb286
Markus Heiser <markus.heiser@darmarit.de>
2021-09-04 08:39:45 +0200

    [fix] typo in searx.webadapter.parse_lang
    Closes #2961
    
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

--- searx/webadapter.py
+++ searx/webadapter.py
@@ -50,7 +50,7 @@ def parse_pageno(form: Dict[str, str]) -> int:
 
 def parse_lang(preferences: Preferences, form: Dict[str, str], raw_text_query: RawTextQuery) -> str:
     if is_locked('language'):
-        return preferences.get_value('langueage')
+        return preferences.get_value('language')
     # get language
     # set specific language if set on request, query or preferences
     # TODO support search with multible languages
