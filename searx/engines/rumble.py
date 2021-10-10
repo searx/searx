79dc10e3828cc812b692572551e5e8eb5a7c1d38
Allen <64094914+allendema@users.noreply.github.com>
2021-10-10 16:44:21 +0200

    [fix] Update about section of Invidious and Rumble + Change filtron error wording (#2959)
    * [fix] Update about section of Invidious
    
    Another website and new documentation
    
    * [fix] Correct engine name in for Rumble (#11)
    
    * [fix] Wording for Filtron error message (#12)

--- searx/engines/rumble.py
+++ searx/engines/rumble.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 """
- Wikipedia (Web
+ Rumble (Videos)
 """
 from urllib.parse import urlencode
 from lxml import html
--- utils/filtron.sh
+++ utils/filtron.sh
@@ -218,7 +218,7 @@ install_all() {
     wait_key
     echo
     if ! service_is_available "http://${FILTRON_LISTEN}" ; then
-        err_msg "Filtron does not listening on: http://${FILTRON_LISTEN}"
+        err_msg "Filtron is not listening on: http://${FILTRON_LISTEN}"
     fi
     if apache_is_installed; then
         info_msg "Apache is installed on this host."
@@ -341,7 +341,7 @@ EOF
     fi
 
     if ! service_is_available "http://${FILTRON_LISTEN}" ; then
-        err_msg "Filtron does not listening on: http://${FILTRON_LISTEN}"
+        err_msg "Filtron is not listening on: http://${FILTRON_LISTEN}"
     fi
 
     if service_is_available "http://${FILTRON_TARGET}" ; then
